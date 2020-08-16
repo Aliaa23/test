d83f6829647a9add7d11041dead7850c06d2512e
Aliaa23 <69125043+Aliaa23@users.noreply.github.com>
2020-08-16 14:45:46 +0200

    project 1

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        dots: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true,
+                loop: true
+            },
+            600: {
+                items: 2,
+                nav: true,
+                loop: true
+            },
+            1000: {
+                items: 3,
+                nav: true,
+                loop: true,
+                margin: 20
+            }
+        }
+
+    });
+});
